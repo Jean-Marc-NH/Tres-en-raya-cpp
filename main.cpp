dd390f1a9a448a271d2fa5db174ddf152340c53a
NikolasZM <101744161+NikolasZM@users.noreply.github.com>
2022-09-16 13:13:23 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-// #include <string>
-
-#define NC "\e[0m"
-#define RED "\e[0;31m"
-#define GRN "\e[0;32m"
-#define CYN "\e[0;36m"
-#define REDB "\e[41m"
 
 using namespace std;
 
 int main(){
-
 	cout << "   _______________________________________________\n";
 	cout << " /|                                               |\n";
 	cout << "| |           Jean Marc Nadeau Huamani            |\n";
-	cout << "| |         Nikolas Ymanol Zuniga Monroy          |\n";
+	cout << "| |         Nikolas Ymanol Zuñiga Monroy          |\n";
 	cout << "| |                     CComp1                    |\n";
 	cout << "| |_______________________________________________|\n";
 	cout << "|/________________________________________________/\n\n";
@@ -30,21 +22,18 @@ int main(){
 	char p7{'7'};
 	char p8{'8'};
 	char p9{'9'};
-	char x{'+'};
-	
-	int casilla{0};
-	bool c{true};
+	char x{' '};
 	int a{0};
-	// cout << "Coloque el nombre del primer jugador: ";
-	// cin >> j2;
-	// cout <<"\nColoque el nombre del segundo jugador: ";
-	// cin >> j1;
-
-	cout <<"Jugadores: \n" << " -" << "jugador 1 (X)" << "\n" << " -" << "jugador 2 (O)" << "\n\n";
-
-	while(c){
-
-
+	char a1{'X'};
+	char a2{'O'};
+	cout<<"Jugador 1 escoja su ficha: ";
+	cin>>a1;
+	cout<<"jugador 2 escoja su ficha: ";
+	cin>>a2;
+	while(true){
+		if (a == 9){
+			break;
+		} 
 		if(((p1 == p2)&&(p2==p3))||((p4==p5)&&(p5==p6))||((p7==p8)&&(p8==p9))||((p1==p5)&&(p5==p9))||((p1==p4)&&(p4==p7))||((p2==p5)&&(p5==p8))||((p3==p6)&&(p6==p9))){
 			cout << "El juego termino el ganador es: ";
 			if(a%2 != 0){
@@ -53,30 +42,20 @@ int main(){
 				cout << "jugador 2" << "\n" ;
 			}
 			break;
-		}
-
-
-		if (a == 9){
-			cout << "Felicidades es un empate";
-			break;
-		} 
-
-		
-		cout << p1 << " | " << p2 << " | " << p3 << "\n";
+		}		
+		cout <<endl<< p1 << " | " << p2 << " | " << p3 << "\n";
 		cout << "---------\n";
 		cout << p4 << " | " << p5 << " | " << p6 << "\n";
 		cout << "---------\n";
 		cout << p7 << " | " << p8 << " | " << p9 << "\n";
-		
-		if(a%2 != 0){
-			cout << "\nTurno de jugador 2 " << "\n\n";
-			cin >> p;
-			x='O';
-		}else{
-			cout << "\nTurno de juagador 1 " << "\n\n";
-			cin >> p;
-			x='X';
-		}
+		if(a%2==0){
+			cout<<"Turno del jugador 1("<<a1<<"): ";
+			x=a1;
+		}	else{
+			cout<<"Turno del jugador 2("<<a2<<"):  ";
+			x=a2;
+			}
+		if (cin>>p){
 
 			if((p == 1)&&(p1=='1'))	{	//Ambas condicionales se juntan en una sola
 			p1=x;
@@ -96,36 +75,20 @@ int main(){
 			p8=x;
 		}	else if((p == 9)&&(p9=='9'))	{
 			p9=x;
-		}	else if((p1!='1')||(p1!='2')||(p1!='3')||(p1!='4')||(p1!='5')||(p1!='6')||(p1!='7')||(p1!='8')||(p1!='9')){
-			cout << "Elija una casilla valida\n";
+		}else {
+			cout << "Seleccione casilla valida";
 			continue;
-		}	else if((p != 1)&&(p != 2)&&(p != 3)&&(p != 4)&&(p != 5)&&(p != 6)&&(p != 7)&&(p != 8)&&(p != 9)){
-			cout << "Elija una casilla valida\n";
-			continue;
-		}	else {
-			cout << "Valor invalido\n";
-			break;
 		}
-
-
-
-		if(c == true){
-			++a;
-		}
-
-	}
-
-	if ((a == 9)||((p1 == p2)&&(p2==p3))||((p4==p5)&&(p5==p6))||((p7==p8)&&(p8==p9))||((p1==p5)&&(p5==p9))||((p1==p4)&&(p4==p7))||((p2==p5)&&(p5==p8))||((p3==p6)&&(p6==p9))){
-		cout << "Tablero final: \n";
-		cout << p1 << " | " << p2 << " | " << p3 << "\n";
+}	else{
+	cout<<"valor invalido";
+	break;
+}
+		++a;
+		cout <<endl<< p1 << " | " << p2 << " | " << p3 << "\n";
 		cout << "---------\n";
 		cout << p4 << " | " << p5 << " | " << p6 << "\n";
 		cout << "---------\n";
 		cout << p7 << " | " << p8 << " | " << p9 << "\n";
 	}
-
-	cout << "\nfin";
-
+cout << "\nfin";
 }
-
-
